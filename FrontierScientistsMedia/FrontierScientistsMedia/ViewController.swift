5f1b4ff77ed8f0659e7664eef86866736620a90d
Jay Byam <jrbyam@alaska.edu>
2015-05-21 09:28:44 -0800

    Titles and icons added to main menu. Main background in place

--- FrontierScientistsMedia/FrontierScientistsMedia/ViewController.swift
+++ FrontierScientistsMedia/FrontierScientistsMedia/ViewController.swift
@@ -8,23 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet var iconImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+}
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var mainMenu: UITableView!
+    @IBOutlet var bindingBack: UIView!
+    @IBOutlet var pageBack: UIView!
+    @IBOutlet var shadow: UIImageView!
+    
     var sections = ["Research", "Videos", "Maps", "Articles", "Ask a Scientist", "About"]
+    var icons = ["research_icon.png", "video_icon.png", "map_icon.png", "article_icon.png", "ask_a_scientist_icon.png", "about_icon.png"]
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sections.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "section")
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "section")
+        cell.imageView?.layoutMargins = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 10)
+        cell.imageView?.image = UIImage(named: icons[indexPath.row])
         cell.textLabel?.text = sections[indexPath.row]
+        cell.backgroundColor = UIColor.clearColor()
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Frontier Scientists"
+        mainMenu.rowHeight = 60
+        mainMenu.separatorColor = UIColor.blackColor()
+        mainMenu.separatorInset = UIEdgeInsetsZero
+        mainMenu.backgroundColor = UIColor.clearColor()
+        shadow.backgroundColor = UIColor(patternImage: UIImage(named: "drawer_shadow.png")!)
+        bindingBack.backgroundColor = UIColor(patternImage: UIImage(named: "navigation_bg.jpg")!)
+        pageBack.backgroundColor = UIColor(patternImage: UIImage(named: "page.jpeg")!)
     }
 
     override func didReceiveMemoryWarning() {
