cb36329582744db02c64e5a8046052e09ed0437b
sfarabaugh <sfarabaugh@alaska.edu>
2015-06-11 13:17:03 -0800

    Fixed layout for ask a scientist

---
+++ FrontierScientistsMedia/FrontierScientistsMedia/aboutViewController.swift
@@ -0,0 +1,14 @@
+//
+//  aboutViewController.swift
+//  FrontierScientistsMedia
+//
+//  Created by sfarabaugh on 6/9/15.
+//  Copyright (c) 2015 FrontierScientists. All rights reserved.
+//
+
+import Foundation
+
+class aboutViewController: UITableViewController {
+
+    
+}
