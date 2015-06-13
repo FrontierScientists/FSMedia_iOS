443496b0c87f561747bd00b8ff842e91fe0d7630
Jay Byam <jrbyam@alaska.edu>
2015-06-12 16:15:18 -0800

    Fixed the gap in the waves and did more work on the tap screen

--- FrontierScientistsMedia/FrontierScientistsMedia/GameViewController.swift
+++ FrontierScientistsMedia/FrontierScientistsMedia/GameViewController.swift
@@ -30,7 +30,6 @@ class GameViewController: UIViewController {
     var scene: GameScene!
     var scene2: GameScene2!
     @IBOutlet weak var tapScreen: UIView!
-    @IBOutlet weak var tapIcon: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,28 +48,49 @@ class GameViewController: UIViewController {
         scene.paused = true
         
         // Format the tutorial tap screen
-        tapIcon.image = UIImage(named: "Game/tap.png")
+        // Add in the "Tap to start" icon
+//        var startTapIcon = UIImageView(image: UIImage(named: "Game/tap.png"))
+//        startTapIcon.frame = CGRectMake(self.view.frame.width/2 - (self.view.frame.width/3)/2 - 1, (self.view.frame.width/3)/2, self.view.frame.width/3 - 2, self.view.frame.width/3 - 2)
+//        tapScreen.addSubview(startTapIcon)
+        // Add in the divider display lines
         var leftTutorialDivider = UIView(frame: CGRectMake(self.view.frame.width/3, 0, 1, self.view.frame.height))
         var rightTutorialDivider = UIView(frame: CGRectMake((self.view.frame.width/3)*2, 0, 1, self.view.frame.height))
         leftTutorialDivider.backgroundColor = UIColor.grayColor()
         rightTutorialDivider.backgroundColor = UIColor.grayColor()
         tapScreen.addSubview(leftTutorialDivider)
         tapScreen.addSubview(rightTutorialDivider)
+        // Add in the arrows, tap icons and uav icons for the flying directions
         var leftTapIcon = UIImageView(image: UIImage(named: "Game/finger.png"))
         var leftArrow = UIImageView(image: UIImage(named: "Game/leftArrow.png"))
         var leftUAVIcon = UIImageView(image: UIImage(named: "Game/tutorialUAVIcon.png")?.imageRotatedByDegrees(-15, flip: false))
+        var upTapIcon = UIImageView(image: UIImage(named: "Game/finger.png"))
+        var upArrow = UIImageView(image: UIImage(named: "Game/upArrow.png"))
+        var upUAVIcon = UIImageView(image: UIImage(named: "Game/tutorialUAVIcon.png"))
         var rightTapIcon = UIImageView(image: UIImage(named: "Game/finger.png"))
         var rightArrow = UIImageView(image: UIImage(named: "Game/rightArrow.png"))
         var rightUAVIcon = UIImageView(image: UIImage(named: "Game/tutorialUAVIcon.png")?.imageRotatedByDegrees(15, flip: false))
-        leftTapIcon.frame = CGRectMake(self.view.frame.width/6 + 1, (self.view.frame.height/4)*3, self.view.frame.width/6 - 2, self.view.frame.width/6 - 2)
-        leftArrow.frame = CGRectMake(1, (self.view.frame.height/4)*3, self.view.frame.width/6 - 2, self.view.frame.width/6 - 2)
-        leftUAVIcon.frame = CGRectMake(self.view.frame.width/6 - (self.view.frame.width/3 - 2)/2, (self.view.frame.height/4)*3 - (self.view.frame.width/4 + 2), self.view.frame.width/3 - 2, self.view.frame.width/4)
-        rightTapIcon.frame = CGRectMake((self.view.frame.width/3)*2 + 1, (self.view.frame.height/4)*3, self.view.frame.width/6 - 2, self.view.frame.width/6 - 2)
-        rightArrow.frame = CGRectMake(self.view.frame.width - self.view.frame.width/6 + 1, (self.view.frame.height/4)*3, self.view.frame.width/6 - 2, self.view.frame.width/6 - 2)
-        rightUAVIcon.frame = CGRectMake(self.view.frame.width - self.view.frame.width/6 - (self.view.frame.width/3 - 2)/2, (self.view.frame.height/4)*3 - (self.view.frame.width/4 + 2), self.view.frame.width/3 - 2, self.view.frame.width/4)
+        
+        let sixthWidth = self.view.frame.width/6
+        let fourthWidth = self.view.frame.width/4
+        let thridWidth = self.view.frame.width/3
+        let halfWidth = self.view.frame.width/2
+        let fourthHeight = self.view.frame.height/4
+        
+        leftTapIcon.frame = CGRectMake(sixthWidth + 1, fourthHeight*3, sixthWidth - 2, sixthWidth - 2)
+        leftArrow.frame = CGRectMake(1, fourthHeight*3, sixthWidth - 2, sixthWidth - 2)
+        leftUAVIcon.frame = CGRectMake(sixthWidth/2 + 1, fourthHeight*3 - (fourthWidth/2 + 2), sixthWidth - 2, fourthWidth/2)
+        upTapIcon.frame = CGRectMake(halfWidth - (sixthWidth - 2)/2, fourthHeight*3, sixthWidth - 2, sixthWidth - 2)
+        upArrow.frame = CGRectMake(halfWidth - (sixthWidth - 2)/2, fourthHeight*3 - sixthWidth - 2, sixthWidth - 2, sixthWidth - 2)
+        upUAVIcon.frame = CGRectMake(halfWidth - sixthWidth/2, fourthHeight*3 - (sixthWidth - 2)*2 - 2, sixthWidth - 2, sixthWidth/2)
+        rightTapIcon.frame = CGRectMake(thridWidth*2 + 1, fourthHeight*3, sixthWidth - 2, sixthWidth - 2)
+        rightArrow.frame = CGRectMake(self.view.frame.width - sixthWidth + 1, fourthHeight*3, sixthWidth - 2, sixthWidth - 2)
+        rightUAVIcon.frame = CGRectMake(self.view.frame.width - sixthWidth - (sixthWidth/2 + 1), fourthHeight*3 - (fourthWidth/2 + 2), sixthWidth - 2, fourthWidth/2)
         tapScreen.addSubview(leftTapIcon)
         tapScreen.addSubview(leftArrow)
         tapScreen.addSubview(leftUAVIcon)
+        tapScreen.addSubview(upTapIcon)
+        tapScreen.addSubview(upArrow)
+        tapScreen.addSubview(upUAVIcon)
         tapScreen.addSubview(rightTapIcon)
         tapScreen.addSubview(rightArrow)
         tapScreen.addSubview(rightUAVIcon)
